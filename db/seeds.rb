fd0cd2f9a9e26da7f666f3a5a19522eb2a84a82c
Remi Juhe <remi.juhe@edu.escpeurope.eu>
2018-08-16 16:25:11 +0200

    backend complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 puts "Generate ingredients"
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+mint_leaves = Ingredient.create(name: "mint leaves")
+
+mojito = Cocktail.create(name: "Mojito")
+sex_on_the_bitch = Cocktail.create(name: "Sex on The Bitch")
+pina_colade = Cocktail.create(name: "Pina Colada")
+
+Dose.create(description: "6 cL", cocktail_id: mojito[:id], ingredient_id: lemon[:id])
 
 puts "C'est fini"
